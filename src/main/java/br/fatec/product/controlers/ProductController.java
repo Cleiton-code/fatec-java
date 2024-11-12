a8a8348580f5d12a2b4e6f891c4785e5ee629d89
cleiton valentim <cleiton_valentim@hotmail.com.br>
2024-11-12 10:57:11 -0300

    Class - 3

--- src/main/java/br/fatec/product/controlers/ProductController.java
+++ src/main/java/br/fatec/product/controlers/ProductController.java
@@ -3,8 +3,13 @@ package br.fatec.product.controlers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,17 +19,37 @@ import br.fatec.product.service.ProductService;
 @RestController
 @RequestMapping("products")
 public class ProductController {
+   
     @Autowired
     private ProductService service;
+    
     @GetMapping
-    public List<Product> geProducts(){
-            return service.getAlProductsl();
+    public ResponseEntity<List<Product>> geProducts(){
+            return ResponseEntity.ok(service.getAlProductsl());
         
     }
-    @GetMapping("(id)")
-    public Product geProduct(@PathVariable long id){
-            return service.getProductByIProduct(id);
+    @GetMapping("{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable long id){
+            return ResponseEntity.ok(service.getProductByIProduct(id));
 
     }
     
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void>deleteProduct(@PathVariable long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+        
+    }
+
+    @PutMapping()
+    public ResponseEntity<Void> updateProduct(@PathVariable long id, @RequestBody Product product){
+        service.update(product, id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping()
+    public ResponseEntity<Product> saveProduct(@RequestBody Product product){
+        Product newProduct = service.save(product);
+        return ResponseEntity.created(null).body(newProduct);
+    }
 }
